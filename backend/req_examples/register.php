48786e02bc8ea512a0f5d3f60c32eef2d3728261
lumahloi <magusoflowers0803@gmail.com>
2025-06-04 21:45:41 -0300

    feat req_examples/register.php

---
+++ backend/req_examples/register.php
@@ -0,0 +1,21 @@
+<?php
+$data = [
+    'fullname' => 'Maria Silva',
+    'email' => 'maria@email.com',
+    'telephone' => '11999990000',
+    'password' => '12345678',
+    'confirmPassword' => '12345678'
+];
+
+$options = [
+    'http' => [
+        'header'  => "Content-type: application/x-www-form-urlencoded",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+        'ignore_errors' => true
+    ]
+];
+
+$context  = stream_context_create($options);
+$result = file_get_contents('http://localhost/brincaqui/backend/auth/register.php', false, $context);
+echo $result;
