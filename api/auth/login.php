051b2f73abbeb37bd89473e653a3173e8cf0acca
lumahloi <magusoflowers0803@gmail.com>
2025-06-05 22:23:08 -0300

    rename vars to input_

--- api/auth/login.php
+++ api/auth/login.php
@@ -8,31 +8,33 @@ require_once BASE_DIR . "/utils/validate_infos.php";
 switch ($_SERVER['REQUEST_METHOD'])
 {
   case 'POST': 
-    $email = filter_input(INPUT_POST, "email");
-    $password = filter_input(INPUT_POST, "password");
+    $input_email = filter_input(INPUT_POST, "email");
+    $input_password = filter_input(INPUT_POST, "password");
     
-    valid_email($email);
-    valid_password($password);
+    valid_email($input_email);
+    valid_password($input_password);
     
-    if (!unique_email($email)) {
+    if (!unique_email($input_email)) {
       response_format(400, "Não existe usuário cadastrado com este e-mail.");
       exit;
     }
     
     $pdo = DbConnection::connect();
     $p_check_password = $pdo->prepare("SELECT user_password FROM brincaqui.usuario WHERE user_email = :user_email;");
-    $p_check_password->bindParam(":user_email", $email, PDO::PARAM_STR);
+    $p_check_password->bindParam(":user_email", $input_email, PDO::PARAM_STR);
     $p_check_password->execute();
     $password_from_db = $p_check_password->fetch(PDO::FETCH_ASSOC);
-    if (!$password_from_db || !password_verify($password, $password_from_db['user_password'])) {
+    
+    if (!$password_from_db || !password_verify($input_password, $password_from_db['user_password'])) {
       response_format(400, "Senha inválida.");
       exit;
     }
     
     $p_get_user_info = $pdo->prepare("SELECT user_id, user_name, user_type FROM brincaqui.usuario WHERE user_email = :user_email;");
-    $p_get_user_info->bindParam(":user_email", $email, PDO::PARAM_STR);
+    $p_get_user_info->bindParam(":user_email", $input_email, PDO::PARAM_STR);
     $p_get_user_info->execute();
     $user_info = $p_get_user_info->fetch(PDO::FETCH_ASSOC);
+
     $_SESSION["user_id"] = $user_info['user_id'];
     $_SESSION["user_type"] = $user_info['user_type'];
     
--- api/auth/register.php
+++ api/auth/register.php
@@ -9,48 +9,51 @@ switch ($_SERVER['REQUEST_METHOD'])
   case 'POST': 
     date_default_timezone_set('America/Sao_Paulo');
 
-    $fullname = filter_input(INPUT_POST, "fullname");
-    $email = filter_input(INPUT_POST, "email");
-    $telephone = filter_input(INPUT_POST, "telephone");
-    $password = filter_input(INPUT_POST, "password");
-    $confirmPassword = filter_input(INPUT_POST, "confirmPassword");
-    $userType = filter_input(INPUT_POST, "userType");
+    $input_fullname = filter_input(INPUT_POST, "fullname");
+    $input_email = filter_input(INPUT_POST, "email");
+    $input_telephone = filter_input(INPUT_POST, "telephone");
+    $input_password = filter_input(INPUT_POST, "password");
+    $input_confirm_password = filter_input(INPUT_POST, "confirmPassword");
+    $input_user_type = filter_input(INPUT_POST, "userType");
 
-    valid_fullname($fullname);
-    valid_telephone($telephone);
-    valid_email($email);
-    valid_password($password);
-    valid_user_type($userType);
+    valid_fullname($input_fullname);
+    valid_telephone($input_telephone);
+    valid_email($input_email);
+    valid_password($input_password);
+    valid_user_type($input_user_type);
 
-    if ($password !== $confirmPassword) {
+    if ($input_password !== $input_confirm_password) {
       response_format(400, "As senhas não coincidem.");
       exit;
     }
 
-    if (unique_telephone($telephone)) {
+    if (unique_telephone($input_telephone)) {
       response_format(400, "Já existe um usuário cadastrado com este telefone.");
       exit;
     }
 
-    if (unique_email($email)) {
+    if (unique_email($input_email)) {
       response_format(400, "Já existe um usuário cadastrado com este e-mail.");
       exit;
     }
 
-    $fullname = preg_replace('/[^a-zA-ZÀ-ÿ\s]/u', '', $fullname);
-    $telephone = preg_replace('/\D/', '', $telephone);
-    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $input_fullname = preg_replace('/[^a-zA-ZÀ-ÿ\s]/u', '', $input_fullname);
+
+    $input_telephone = preg_replace('/\D/', '', $input_telephone);
+
+    $hash = password_hash($input_password, PASSWORD_DEFAULT);
+
     $date = date('Y/m/d');
 
     $pdo = DbConnection::connect();
     $p_insert = $pdo->prepare("INSERT INTO brincaqui.usuario (user_name, user_telephone, user_email, user_password, user_active, user_creation, user_lastedit, user_type) VALUES (:user_name, :user_telephone, :user_email, :user_password, 1, :user_creation, :user_lastedit, :user_type);");
-    $p_insert->bindParam(":user_name", $fullname, PDO::PARAM_STR);
-    $p_insert->bindParam(":user_telephone", $telephone, PDO::PARAM_STR);
-    $p_insert->bindParam(":user_email", $email, PDO::PARAM_STR);
+    $p_insert->bindParam(":user_name", $input_fullname, PDO::PARAM_STR);
+    $p_insert->bindParam(":user_telephone", $input_telephone, PDO::PARAM_STR);
+    $p_insert->bindParam(":user_email", $input_email, PDO::PARAM_STR);
     $p_insert->bindParam(":user_password", $hash, PDO::PARAM_STR);
     $p_insert->bindParam(":user_creation", $date, PDO::PARAM_STR);
     $p_insert->bindParam(":user_lastedit", $date, PDO::PARAM_STR);
-    $p_insert->bindParam(":user_type", $userType, PDO::PARAM_STR);
+    $p_insert->bindParam(":user_type", $input_user_type, PDO::PARAM_STR);
     $insert_user = $p_insert->execute();
 
     if (!$insert_user) {
