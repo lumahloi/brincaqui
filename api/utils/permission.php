f283006bb44d7afc3c31deaee2e9ddd71777aef3
lumahloi <magusoflowers0803@gmail.com>
2025-06-06 01:12:28 -0300

    fix permission.php

--- api/utils/permission.php
+++ api/utils/permission.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
 
 function check_permission($expected_types)
 {
     if (!isset($_SESSION['user_type']) || !in_array($_SESSION['user_type'], (array)$expected_types)) {
         response_format(403, "Acesso negado.");
+        echo $_SESSION['user_type'];
         exit;
     }
 }
