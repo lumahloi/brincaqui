783b4975003a2b8846f37875645ce8414fdc235a
lumahloi <magusoflowers0803@gmail.com>
2025-06-09 20:04:17 -0300

    feat post auth/logout.php

---
+++ api/auth/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once "../base_dir.php";
+require_once BASE_DIR . "/utils/response_format.php";
+require_once BASE_DIR . "/utils/permission.php";
+check_permission([1,2,3], $cookie);
+
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'POST':
+    session_destroy();
+    response_format(200, "Até mais!!");
+    break;
+
+  default:
+    response_format(405, "Apenas POST permitido.");
+}
