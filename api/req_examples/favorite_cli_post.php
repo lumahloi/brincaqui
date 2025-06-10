24ab4c910975b2b0e3e45327f2b77625d48a1a6e
lumahloi <magusoflowers0803@gmail.com>
2025-06-09 21:58:13 -0300

    feat test favorite_cli_post

---
+++ api/req_examples/favorite_cli_post.php
@@ -0,0 +1,15 @@
+<?php
+require_once "test_auth_header.php";
+
+$ch = curl_init('http://localhost/brincaqui/api/favorite/8');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+    "Cookie: $cookie"
+]);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
