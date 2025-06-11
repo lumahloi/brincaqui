272ae0b67c3c2b6ba84252c822d9c65bc995afc5
lumahloi <magusoflowers0803@gmail.com>
2025-06-11 15:08:20 -0300

    feat phpunit

--- api/req_examples/password_hash.php
+++
@@ -1,4 +0,0 @@
-<?php
-$senha = 'admin@123';
-$hash = password_hash($senha, PASSWORD_DEFAULT);
-echo $hash;
--- api/req_examples/test_auth_header.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "../base_dir.php";
-require_once BASE_DIR . "/utils/load_env.php";
-load_env(BASE_DIR . '/.env');
-session_id(getenv('SESSION_ID'));
-session_start();
-$cookie = "PHPSESSID=" . session_id();
