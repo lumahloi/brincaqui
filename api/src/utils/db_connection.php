c3fe4f9503dcdc96a5f94f1e4f75f715f6f582cc
lumahloi <magusoflowers0803@gmail.com>
2025-06-11 20:49:55 -0300

    trycatch

--- api/src/utils/db_connection.php
+++ api/src/utils/db_connection.php
@@ -6,9 +6,13 @@ abstract class DbConnection
 {
   public static function connect()
   {
-    $dsn = 'mysql:' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME') . ';charset=utf8mb4';
-    $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASSWORD'));
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
+    try {
+      $dsn = 'mysql:' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME') . ';charset=utf8mb4';
+      $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASSWORD'));
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $pdo;
+    } catch (PDOException $e) {
+      throw $e;
+    }
   }
 }
