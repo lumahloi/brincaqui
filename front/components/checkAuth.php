cf55f1f0fef95d44858f4c295149e7bad9bdff0f
lumahloi <magusoflowers0803@gmail.com>
2025-06-14 11:20:48 -0300

    fix checkauth

--- front/components/checkAuth.php
+++ front/components/checkAuth.php
@@ -8,15 +8,3 @@ if (!isset($_SESSION['user_id'])) {
   http_response_code(302);
   exit;
 }
-
-require_once BASE_DIR . "/components/header.php";
-require_once BASE_DIR . "/components/checkAuth.php";
-?>
-</head>
-
-<body>
-  <p>página inicial</p>
-
-  <?php require_once BASE_DIR . "/components/modal.php"; ?>
-</body>
-</html>
