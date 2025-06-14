10dfa5ece71dfd1b596bcce73fb7031399303c89
lumahloi <magusoflowers0803@gmail.com>
2025-06-14 14:23:58 -0300

    fix prototype

--- front/components/feedCliente.php
+++ front/components/feedCliente.php
@@ -1,16 +1,56 @@
-<form>
-  <div class="mb-3">
-    <label for="form-place" class="form-label">Lugar</label>
-    <input type="text" class="form-control" id="form-place" placeholder="Rua das Palmeiras">
-  </div>
-  <div class="mb-3">
-    <button type="button" class="btn btn-primary form-control" id="form-submit">Procurar</button>
+<form id="form-filters" class="mb-4">
+  <div class="row g-2">
+    <div class="col-md-3">
+      <input type="text" class="form-control" name="commodities" placeholder="Commodities">
+    </div>
+    <div class="col-md-3">
+      <input type="text" class="form-control" name="discounts" placeholder="Discounts">
+    </div>
+    <div class="col-md-3">
+      <input type="text" class="form-control" name="ages" placeholder="Ages">
+    </div>
+    <div class="col-md-3">
+      <input type="text" class="form-control" name="cep" placeholder="CEP">
+    </div>
+    <div class="col-md-3">
+      <input type="text" class="form-control" name="city" placeholder="City">
+    </div>
+    <div class="col-md-3">
+      <input type="text" class="form-control" name="neighborhood" placeholder="Neighborhood">
+    </div>
+    <div class="col-md-3">
+      <input type="text" class="form-control" name="state" placeholder="State">
+    </div>
+    <div class="col-md-3">
+      <input type="text" class="form-control" name="country" placeholder="Country">
+    </div>
+    <div class="col-md-3">
+      <select class="form-select" name="order_by">
+        <option value="name">Nome</option>
+        <option value="grade">Nota</option>
+        <option value="faves">Favoritos</option>
+        <option value="visits">Visitas</option>
+      </select>
+    </div>
+    <div class="col-md-3">
+      <select class="form-select" name="order_dir">
+        <option value="asc">Ascendente</option>
+        <option value="desc">Descendente</option>
+      </select>
+    </div>
+    <div class="col-md-3">
+      <input type="number" class="form-control" name="per_page" placeholder="Resultados por página" value="10">
+    </div>
+    <div class="col-md-3">
+      <input type="number" class="form-control" name="page" placeholder="Página" value="0">
+    </div>
+    <div class="col-md-12">
+      <button type="submit" class="btn btn-primary form-control">Procurar</button>
+    </div>
   </div>
 </form>
 
-<div id="results">
-
-</div>
+<div id="results"></div>
 
 <script src="<?php echo BASE_URL ?>/scripts/errorValidation.js"></script>
 <script src="<?php echo BASE_URL ?>/apikey.js"></script>
--- front/scripts/apiGetPlay.js
+++ front/scripts/apiGetPlay.js
@@ -1,17 +1,23 @@
-$("#form-submit").click(function (event) {
+$("#form-filters").submit(function (event) {
   event.preventDefault();
-  // var input_email = $("#form-place").val();
+
+  const params = {};
+  $(this).serializeArray().forEach(({ name, value }) => {
+    if (value) params[name] = value;
+  });
 
   $.ajax({
     type: "GET",
     url: SERVER_URL + "play",
-    contentType: "application/json",
-    // data: JSON.stringify({
-    //   email: input_email
-    // }),
+    data: params,
     success: (response) => {
-      let container = $("#results");
-      container.html(""); 
+      const container = $("#results");
+      container.empty();
+
+      if (!response.return || response.return.length === 0) {
+        container.html("<p class='text-muted'>Nenhum resultado encontrado.</p>");
+        return;
+      }
 
       response.return.forEach(function (item) {
         $.get("front/components/playCard.html", function (template) {
