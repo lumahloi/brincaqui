cb37e167decbea2d65084da65e434c4ea4f15f45
lumahloi <magusoflowers0803@gmail.com>
2025-06-15 12:34:20 -0300

    feat front: get play with filters

--- front/components/feedCliente.php
+++ front/components/feedCliente.php
@@ -10,19 +10,7 @@
       <input type="text" class="form-control" name="ages" placeholder="Ages">
     </div>
     <div class="col-md-3">
-      <input type="text" class="form-control" name="cep" placeholder="CEP">
-    </div>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="city" placeholder="City">
-    </div>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="neighborhood" placeholder="Neighborhood">
-    </div>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="state" placeholder="State">
-    </div>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="country" placeholder="Country">
+      <input type="text" class="form-control" name="address" placeholder="Endereço">
     </div>
     <div class="col-md-3">
       <select class="form-select" name="order_by">
@@ -38,12 +26,6 @@
         <option value="desc">Descendente</option>
       </select>
     </div>
-    <div class="col-md-3">
-      <input type="number" class="form-control" name="per_page" placeholder="Resultados por página" value="10">
-    </div>
-    <div class="col-md-3">
-      <input type="number" class="form-control" name="page" placeholder="Página" value="0">
-    </div>
     <div class="col-md-12">
       <button type="submit" class="btn btn-primary form-control">Procurar</button>
     </div>
@@ -53,6 +35,5 @@
 <div id="results"></div>
 
 <script src="<?php echo BASE_URL ?>/scripts/errorValidation.js"></script>
-<script src="<?php echo BASE_URL ?>/apikey.js"></script>
 <script src="<?php echo BASE_URL ?>/scripts/localizator.js"></script>
 <script src="<?php echo BASE_URL ?>/scripts/apiGetPlay.js"></script>
--- front/scripts/apiGetPlay.js
+++ front/scripts/apiGetPlay.js
@@ -22,7 +22,22 @@ $("#form-filters").submit(function (event) {
       response.return.forEach(function (item) {
         $.get("front/components/playCard.html", function (template) {
           let $card = $(template);
-          $card.find(".title").text(item.brin_id);
+          $card.find("#play-name").text(item.brin_name);
+          $card.find("#play-pictures").text(item.brin_pictures);
+          $card.find("#play-grade").text(item.brin_grade);
+          $card.find("#play-socials").text(item.brin_socials);
+          $card.find("#play-description").text(item.brin_description);
+          $card.find("#play-times").text(item.brin_times);
+          $card.find("#play-commodities").text(item.brin_commodities);
+          $card.find("#play-prices").text(item.brin_prices);
+          $card.find("#play-discounts").text(item.brin_discounts);
+          $card.find("#play-telephone").text(item.brin_telephone);
+          $card.find("#play-email").text(item.brin_email);
+          $card.find("#play-cnpj").text(item.brin_cnpj);
+          $card.find("#play-ages").text(item.brin_ages);
+          $card.find("#play-owner").text(item.Usuario_user_id);
+          $card.find("#play-faves").text(item.brin_faves);
+          $card.find("#play-visits").text(item.brin_visits);
           container.append($card);
         });
       });
