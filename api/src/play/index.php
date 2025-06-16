556247a6e75d06f9308347f17690b0095d31c981
lumahloi <magusoflowers0803@gmail.com>
2025-06-16 11:39:01 -0300

    fix api/play

--- api/src/play/index.php
+++ api/src/play/index.php
@@ -5,18 +5,9 @@ require_once BASE_DIR . "/utils/response_format.php";
 $cookie = filter_var($_COOKIE['PHPSESSID'] ?? '', FILTER_SANITIZE_STRING);
 require_once BASE_DIR . "/utils/permission.php";
 
-switch ($_SESSION['user_type']) {
-  case 1:
-    check_permission([1,3], $cookie);
-    require_once "./components/user_cliente.php";
-    break;
-
-  case 2:
-    check_permission([2,3], $cookie);
-    require_once "./components/user_empresa.php";
-    break;
-
-  default:
-    response_format(400, "Sessão inválida, realize login e tente novamente.");
-    break;
+if(isset($_SESSION['user_type']) && $_SESSION['user_type'] === 2){
+  check_permission([2,3], $cookie);
+  require_once "./components/user_empresa.php";
 }
+
+require_once "./components/user_cliente.php";
