d29b9eb6705dc91695e8a6d3df1755da01b41d00
lumahloi <magusoflowers0803@gmail.com>
2025-06-16 12:11:56 -0300

    fix

---
+++ front/router.php
@@ -0,0 +1,23 @@
+<?php
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
+    return false;
+}
+
+require_once __DIR__ . '/base_dir.php';
+
+$path = $uri;
+
+if ($path === '/' || $path === '') {
+    $path = '/index';
+}
+
+$file = BASE_DIR . '/pages' . $path . '.php';
+
+if (file_exists($file)) {
+    require $file;
+} else {
+    http_response_code(404);
+    echo "Página não encontrada.";
+}
