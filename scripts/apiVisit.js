e40a3fd3ab6f0d39b9e6664c4c0189de911effae
lumahloi <magusoflowers0803@gmail.com>
2025-06-16 21:17:30 -0300

    no double visita

--- scripts/apiVisit.js
+++ scripts/apiVisit.js
@@ -1,13 +1,27 @@
 $("#play").on("click", "#btn-visita", function () {
   const brinId = $(this).data("brin-id");
+
   $.ajax({
-    type: "POST",
+    type: "GET",
     url: SERVER_URL + "visit/" + brinId,
-    success: () => {
-      window.location.href = "/visita?status=sucesso";
+    success: (response) => {
+      if (response.return[0] == null) {
+        $.ajax({
+          type: "POST",
+          url: SERVER_URL + "visit/" + brinId,
+          success: () => {
+            window.location.href = "/visita?status=sucesso";
+          },
+          error: () => {
+            window.location.href = "/visita?status=erro";
+          },
+        });
+      } else {
+        window.location.href = "/visita?status=erro";
+      }
     },
-    error: () => {
-      window.location.href = "/visita?status=erro";
-    }
+    error: (xhr) => {
+      error_validation(xhr);
+    },
   });
 });
