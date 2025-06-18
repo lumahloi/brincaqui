b53b3428cef40ce367d742c9db0b1d3988704753
lumahloi <magusoflowers0803@gmail.com>
2025-06-18 00:30:50 -0300

    section no dinex

---
+++ components/header.php
@@ -0,0 +1,3 @@
+<header class="mb-5">
+  <img src="<?php echo BASE_URL; ?>/img/logo.png" id="logo-header" class="mt-3" alt="BrincAqui">
+</header>
--- pages/index.php
+++ pages/index.php
@@ -6,18 +6,27 @@ require_once BASE_DIR . "/components/head.php";
 <body>
   <div id="initial-options" class="containerw">
     <?php
+    require_once BASE_DIR . "/components/header.php";
     require_once BASE_DIR . "/components/modal.php";
     require_once BASE_DIR . "/components/navigation.php";
     ?>
-    <div class="mb-3">
-      <button type="button" class="btn btn-primary" id="btn-register-type1">Cliente</button>
+    <div class="gradient-1 text-white pt-4">
+      <h3 class="text-center">A diversão está aqui</h3>
+      <div class="row gap-3 p-4">
+        <button type="button" class="btn btn-primary col bg-white border-white text-black" id="btn-register-type1">Criar
+          minha conta</button>
+        <button type="button" class="btn btn-primary col bg-white border-white text-black"
+          id="btn-register-type2">Divulgar meu negócio</button>
+      </div>
     </div>
-    <div class="mb-3">
-      <button type="button" class="btn btn-primary" id="btn-register-type2">Empresa</button>
-    </div>
-    <div class="mb-3">
-      <p><a href="login">Já tenho uma conta</a></p>
-    </div>
+
+    <img src="<?php echo BASE_URL; ?>/img/mae-e-filha.png" alt="" class="img-70 mt-3">
+    <h4 class="text-center mt-4">Aproveite com qualidade</h4>
+    <p class="pe-4 ps-4 text-center">Entre agora e descubra onde estará o próximo momento especial. Temos muitas opções!
+    </p>
+
+    <button href="login" class="btn gradient-1 text-white mx-auto d-block mt-4">Já tenho uma conta</button>
+
   </div>
   <script src="<?php echo BASE_URL ?>/scripts/errorValidation.js"></script>
   <script src="<?php echo BASE_URL ?>/scripts/registerRedirect.js"></script>
